a3ec94214bced14742000e688641228d79da0be6
moisesfilho <moises.son@gmail.com>
2020-08-15 13:52:50 -0300

    Atribuindo parametro com valor default

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,12 +1,18 @@
 export abstract class View<T> {
   private _elemento: JQuery;
+  private _escapar: boolean;
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar: boolean = true) {
     this._elemento = $(seletor);
+    this._escapar = escapar;
   }
 
   update(modelo: T): void {
-    this._elemento.html(this.template(modelo));
+    let template = this.template(modelo);
+    if (this._escapar){
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    }
+    this._elemento.html(template);
   }
 
   abstract template(modelo: T): string;
