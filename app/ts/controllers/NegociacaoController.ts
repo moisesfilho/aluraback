63146009862d8e8bcbb102ada4f29d2f43ee8751
moisesfilho <moises.son@gmail.com>
2020-08-15 18:57:35 -0300

    Utilizando async e await

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -44,25 +44,26 @@ export class NegociacaoController {
   }
 
   @throttle()
-  importaDados() {
-    this._service
-      .obterNegociacoes((res) => {
+  async importaDados() {
+    try {
+      const negociacoesParaImportar = await this._service.obterNegociacoes((res) => {
         if (res.ok) {
           return res;
         } else {
           throw new Error(res.statusText);
         }
-      })
-      .then((negociacoesParaImportar) => {
-        const negocicoesJaImportadas = this._negociacoes.paraArray();
+      });
 
-        negociacoesParaImportar
-          .filter((negociacao) => !negocicoesJaImportadas.some((jaImportada) => negociacao.ehIgual(jaImportada)))
-          .forEach((negociacao) => this._negociacoes.adiciona(negociacao));
+      const negocicoesJaImportadas = this._negociacoes.paraArray();
 
-        this._negociacoesView.update(this._negociacoes);
-      })
-      .catch(err => this._mensagemView.update(err.message));
+      negociacoesParaImportar
+        .filter((negociacao) => !negocicoesJaImportadas.some((jaImportada) => negociacao.ehIgual(jaImportada)))
+        .forEach((negociacao) => this._negociacoes.adiciona(negociacao));
+
+      this._negociacoesView.update(this._negociacoes);
+    } catch (error) {
+      this._mensagemView.update(error.message);
+    }
   }
 }
 
--- app/ts/services/NegociacaoService.ts
+++ app/ts/services/NegociacaoService.ts
@@ -2,7 +2,7 @@ import { Negociacao, NegociacaoParcial } from "../models/index";
 
 export class NegociacaoService {
   obterNegociacoes(handler: HandlerFunction): Promise<Negociacao[]> {
-    return fetch("http://localhost:8080/dados")
+    return fetch("http://localhost:8080/dadosx")
       .then((res) => handler(res))
       .then((res) => res.json())
       .then((dados: NegociacaoParcial[]) => dados.map((dado) => new Negociacao(new Date(), dado.vezes, dado.montante)))
