a30ef65a81587a0977842575c0304d51affcf20c
moisesfilho <moises.son@gmail.com>
2020-08-15 18:46:40 -0300

    Retornando mensagem de falha ao importar

--- app/ts/services/NegociacaoService.ts
+++ app/ts/services/NegociacaoService.ts
@@ -2,11 +2,14 @@ import { Negociacao, NegociacaoParcial } from "../models/index";
 
 export class NegociacaoService {
   obterNegociacoes(handler: HandlerFunction): Promise<Negociacao[]> {
-    return <Promise<Negociacao[]>>fetch("http://localhost:8080/dados")
+    return fetch("http://localhost:8080/dados")
       .then((res) => handler(res))
       .then((res) => res.json())
       .then((dados: NegociacaoParcial[]) => dados.map((dado) => new Negociacao(new Date(), dado.vezes, dado.montante)))
-      .catch((err) => console.log(err.message));
+      .catch((err) => {
+        console.log(err);
+        throw new Error('Não foi possível importar as negociações');
+      });
   }
 }
 
