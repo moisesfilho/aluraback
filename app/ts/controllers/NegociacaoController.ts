c5c5d50c75c51328f50cce0c825b982ae3d1a407
moisesfilho <moises.son@gmail.com>
2020-08-15 14:16:50 -0300

    Usando enum

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -19,11 +19,13 @@ export class NegociacaoController {
   adiciona(event: Event): void {
     event.preventDefault();
 
-    const negociacao = new Negociacao(
-      new Date(this._inputData.val().replace(/-/g, ",")),
-      parseInt(this._inputQuantidade.val()),
-      parseFloat(this._inputValor.val())
-    );
+    let data = new Date(this._inputData.val().replace(/-/g, ","));
+
+    if (!this._ehDiaUtil(data)) {
+      this._mensagemView.update("Negociações só podem ser criadas em dias úteis.");
+    }
+
+    const negociacao = new Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
 
     this._negociacoes.adiciona(negociacao);
 
@@ -36,4 +38,18 @@ export class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     this._mensagemView.update("Negociação adicionada com sucesso!");
   }
+
+  private _ehDiaUtil(data: Date): boolean {
+    return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+  }
+}
+
+enum DiaDaSemana {
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado,
 }
