6bbbb231ffa3942fd6063826d81f61f0179577c3
moisesfilho <moises.son@gmail.com>
2020-08-15 13:29:26 -0300

    Novo esquema de modulos; Importação de classes; Novo esquema de carregamento dos scripts; Aplicação rodando apartir deo server-lite; Execução de compilador e server em paralelo;

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,7 +1,7 @@
-namespace Views {
-  export class MensagemView extends View<string> {
-    template(modelo: string): string {
-      return `<p class="alert alert-info">${modelo}</p>`;
-    }
+import { View } from "./View";
+
+export class MensagemView extends View<string> {
+  template(modelo: string): string {
+    return `<p class="alert alert-info">${modelo}</p>`;
   }
 }
--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,7 +1,9 @@
-namespace Views {
-  export class NegociacoesView extends Views.View<Negociacoes> {
-    template(modelo: Negociacoes): string {
-      return `
+import { View } from "./View";
+import { Negociacoes } from "../models/Negociacoes";
+
+export class NegociacoesView extends View<Negociacoes> {
+  template(modelo: Negociacoes): string {
+    return `
     <table class="table table-hover table-bordered">
         <thead>
             <tr>
@@ -33,6 +35,5 @@ namespace Views {
         </tfoot>
     </table>
     `;
-    }
   }
 }
